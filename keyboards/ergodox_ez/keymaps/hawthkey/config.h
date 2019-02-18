0313a5976cf3041cfaa75bd15a0f42f940aa15b5
John Hawthorn <john@hawthorn.email>
2019-02-17 16:33:34 -0800

    Add hawthkey keympa from configurator

---
+++ keyboards/ergodox_ez/keymaps/hawthkey/config.h
@@ -0,0 +1,14 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef DEBOUNCE
+#define DEBOUNCE 10
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 100
+
+#define PERMISSIVE_HOLD
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+
